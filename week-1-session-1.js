2e1193d03f9899382bd58b59b0f0179ffdebfe12
Taylor J <taylordjone@gmail.com>
2023-06-23 10:55:41 +0200

    Adding Week1 Session1 Problem

---
+++ week-1-session-1.js
@@ -0,0 +1,19 @@
+// Arrange a deck of cards in ascending order
+
+let deck = [];
+const ranks = [
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King",
+];
+const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
