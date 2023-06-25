b6ad8659d4fa4aa381799cd79377700a0c080e56
Taylor J <taylordjone@gmail.com>
2023-06-25 16:27:48 +0200

    adding Classwork section

---
+++ week1ClassWork.js
@@ -0,0 +1,14 @@
+// Week 1 Session 1 Classwork
+
+// Write a function that checks a lists of numbers if a value is a
+// prime number but stops checking if a non-positive value is discovered.
+// Write this function in both function declaration and function
+// expression style.
+
+const test1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const test2 = [24, 12, 32, 13, 22, 23, 21];
+const test3 = [0, 1, 4, 6, 7, 11, 55, 1001, 19];
+
+// Function Declaration
+
+// Function expression Style
